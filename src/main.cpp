d31ed0b3ccfd30d1b370bce81f63a84138fe9ac6
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-19 15:50:47 +0900

    :zap: main.cpp | use vector for vertices

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,31 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graphviz.hpp>
 #include <tuple>
+#include <vector>
 
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> SimpleGraph;
 
 int main() {
     SimpleGraph G;
 
-    auto vertex_left = add_vertex(G);
-    auto vertex_right = add_vertex(G);
+    // Vector for vertices
+    std::vector<SimpleGraph::vertex_descriptor> vertices;
 
+    // Add vertices
+    vertices.push_back(add_vertex(G));
+    vertices.push_back(add_vertex(G));
+
+    // Add edge
     bool added;
-    boost::tie(std::ignore, added) = add_edge(vertex_left, vertex_right, G);
+    boost::tie(std::ignore, added) = add_edge(vertices[0], vertices[1], G);
 
     if (added) {
-        std::cout << "Edge added successfully between" << vertex_left << " and " << vertex_right << std::endl;
+        std::cout << "Edge added successfully between" << vertices[0] << " and " << vertices[1] << std::endl;
     } else {
-        std::cout << "Failed to add edge between" << vertex_left << " and " << vertex_right << std::endl;
+        std::cout << "Failed to add edge between" << vertices[0] << " and " << vertices[1] << std::endl;
     }
 
+    // Output graph as Graphviz format (.dot)
     std::ofstream file("graph.dot");
     boost::write_graphviz(file, G);
 
