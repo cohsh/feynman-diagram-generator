633abb6761a20490efbb26fc907b131c0a15acfe
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-22 11:35:45 +0900

    :zap main.cpp | order of for-loop:

--- src/main.cpp
+++ src/main.cpp
@@ -122,53 +122,23 @@ std::vector<Point> calculate_polygon_vertices(int n, double radius) {
     return vertices;
 }
 
-std::tuple<SimpleGraph, std::vector<SimpleGraph::vertex_descriptor>> get_initial_graph_and_vertices(int order) {
+std::tuple<SimpleGraph, std::vector<SimpleGraph::vertex_descriptor>> get_initial_graph_and_vertices(int number_of_vertices) {
     SimpleGraph G;
 
     // Vector for vertices
     std::vector<SimpleGraph::vertex_descriptor> vertices;
 
-    // Maximum of vertices
-    int max_intermediate_vertices = 2 * (order - 1);
+    std::vector<Point> polygon_vertices = calculate_polygon_vertices(number_of_vertices, 1.0);
 
-    std::vector<Point> polygon_vertices = calculate_polygon_vertices(max_intermediate_vertices + 2, 1.0);
-
-    // Add a vertex for the initial state
-    auto vertex_initial = add_vertex(G);
-    vertices.push_back(vertex_initial);
-    G[vertex_initial].x = polygon_vertices[0].x;
-    G[vertex_initial].y = polygon_vertices[0].y;
-    G[vertex_initial].label = "";
-    G[vertex_initial].size = 0.5;
-    G[vertex_initial].fillcolor = "blue";
-    G[vertex_initial].required_solid_degree = 1;
-    G[vertex_initial].solid_degree = 0;
-    G[vertex_initial].dashed_degree = 0;
-    G[vertex_initial].solid_loop = false;
-    G[vertex_initial].dashed_loop = false;
-
-    // Add a vertex for the final state
-    auto vertex_final = add_vertex(G);
-    vertices.push_back(vertex_final);
-    G[vertex_final].x = polygon_vertices[1].x;
-    G[vertex_final].y = polygon_vertices[1].y;
-    G[vertex_final].label = "";
-    G[vertex_final].size = 0.5;
-    G[vertex_final].fillcolor = "red";
-    G[vertex_final].required_solid_degree = 1;
-    G[vertex_final].solid_degree = 0;
-    G[vertex_final].dashed_degree = 0;
-    G[vertex_final].solid_loop = false;
-    G[vertex_final].dashed_loop = false;
-
-    // Add intermediate vertices
-    for (int i = 0; i < max_intermediate_vertices; ++i) {
+    // Add vertices
+    for (int i = 0; i < number_of_vertices; ++i) {
         auto v = add_vertex(G);
         vertices.push_back(v);
 
-        // Set random coordinates
-        G[v].x = polygon_vertices[i+2].x;
-        G[v].y = polygon_vertices[i+2].y;
+        // Set coordinates
+        G[v].x = polygon_vertices[i].x;
+        G[v].y = polygon_vertices[i].y;
+        // Set label
         G[v].label = "";
         // Set size
         G[v].size = 0.5;
@@ -178,6 +148,18 @@ std::tuple<SimpleGraph, std::vector<SimpleGraph::vertex_descriptor>> get_initial
         G[v].required_solid_degree = 2;
         G[v].solid_degree = 0;
         G[v].dashed_degree = 0;
+        // Set having loop flag
+        G[v].solid_loop = false;
+        G[v].dashed_loop = false;
+    }
+
+    // Set properties for the initial and final states
+    G[vertices[0]].required_solid_degree = 1;
+    G[vertices[0]].fillcolor = "red";
+
+    if (number_of_vertices > 1) {
+        G[vertices[1]].required_solid_degree = 1;
+        G[vertices[1]].fillcolor = "blue";
     }
 
     return std::make_tuple(G, vertices);
@@ -207,6 +189,58 @@ std::unordered_set<SimpleGraph::vertex_descriptor> bfs_reachable_vertices(const 
     return visited;
 }
 
+// BFS to find all vertices reachable from the start vertex via solid edges
+std::unordered_set<SimpleGraph::vertex_descriptor> bfs_reachable_vertices_solid(const SimpleGraph &G, SimpleGraph::vertex_descriptor start) {
+    std::unordered_set<SimpleGraph::vertex_descriptor> visited;
+    std::queue<SimpleGraph::vertex_descriptor> q;
+
+    visited.insert(start);
+    q.push(start);
+
+    while (!q.empty()) {
+        auto u = q.front();
+        q.pop();
+
+        for (auto edge_it = out_edges(u, G); edge_it.first != edge_it.second; ++edge_it.first) {
+            if (G[*edge_it.first].style == "solid") {
+                auto v = target(*edge_it.first, G);
+                if (visited.find(v) == visited.end()) {
+                    visited.insert(v);
+                    q.push(v);
+                }
+            }
+        }
+    }
+
+    return visited;
+}
+
+// BFS to find all vertices reachable from the start vertex via dashed edges
+std::unordered_set<SimpleGraph::vertex_descriptor> bfs_reachable_vertices_dashed(const SimpleGraph &G, SimpleGraph::vertex_descriptor start) {
+    std::unordered_set<SimpleGraph::vertex_descriptor> visited;
+    std::queue<SimpleGraph::vertex_descriptor> q;
+
+    visited.insert(start);
+    q.push(start);
+
+    while (!q.empty()) {
+        auto u = q.front();
+        q.pop();
+
+        for (auto edge_it = out_edges(u, G); edge_it.first != edge_it.second; ++edge_it.first) {
+            if (G[*edge_it.first].style == "dashed") {
+                auto v = target(*edge_it.first, G);
+                if (visited.find(v) == visited.end()) {
+                    visited.insert(v);
+                    q.push(v);
+                }
+            }
+        }
+    }
+
+    return visited;
+}
+
 // DFS to find all vertices reachable from the start vertex
 std::unordered_set<SimpleGraph::vertex_descriptor> dfs_reachable_vertices(const SimpleGraph &G, SimpleGraph::vertex_descriptor start) {
     std::unordered_set<SimpleGraph::vertex_descriptor> visited;
@@ -224,6 +258,58 @@ std::unordered_set<SimpleGraph::vertex_descriptor> dfs_reachable_vertices(const 
             if (visited.find(v) == visited.end()) {
                 visited.insert(v);
                 s.push(v);
+            }
+        }
+    }
+
+    return visited;
+}
+
+// DFS to find all vertices reachable from the start vertex via solid edges
+std::unordered_set<SimpleGraph::vertex_descriptor> dfs_reachable_vertices_solid(const SimpleGraph &G, SimpleGraph::vertex_descriptor start) {
+    std::unordered_set<SimpleGraph::vertex_descriptor> visited;
+    std::stack<SimpleGraph::vertex_descriptor> s;
+
+    visited.insert(start);
+    s.push(start);
+
+    while (!s.empty()) {
+        auto u = s.top();
+        s.pop();
+
+        for (auto edge_it = out_edges(u, G); edge_it.first != edge_it.second; ++edge_it.first) {
+            if (G[*edge_it.first].style == "solid") {
+                auto v = target(*edge_it.first, G);
+                if (visited.find(v) == visited.end()) {
+                    visited.insert(v);
+                    s.push(v);
+                }
+            }
+        }
+    }
+
+    return visited;
+}
+
+// DFS to find all vertices reachable from the start vertex via dashed edges
+std::unordered_set<SimpleGraph::vertex_descriptor> dfs_reachable_vertices_dashed(const SimpleGraph &G, SimpleGraph::vertex_descriptor start) {
+    std::unordered_set<SimpleGraph::vertex_descriptor> visited;
+    std::stack<SimpleGraph::vertex_descriptor> s;
+
+    visited.insert(start);
+    s.push(start);
+
+    while (!s.empty()) {
+        auto u = s.top();
+        s.pop();
+
+        for (auto edge_it = out_edges(u, G); edge_it.first != edge_it.second; ++edge_it.first) {
+            if (G[*edge_it.first].style == "dashed") {
+                auto v = target(*edge_it.first, G);
+                if (visited.find(v) == visited.end()) {
+                    visited.insert(v);
+                    s.push(v);
+                }
             }
         }
     }
@@ -259,27 +345,27 @@ int main(int argc, char* argv[]) {
 
     int max_solid_edges = 2 * order - 1;
 
-    // Create all possible dashed edges
-    std::vector<std::pair<int, int>> all_edges;
+    int max_of_vertices = 2 * order;
 
-    int n = 2 * order;
+    for (int number_of_vertices = 1; number_of_vertices < max_of_vertices + 1; ++number_of_vertices) {
+        // Create all possible edges
+        std::vector<std::pair<int, int>> all_edges;
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = i; j < n; ++j) {
-            all_edges.push_back({i, j});
+        for (int i = 0; i < number_of_vertices; ++i) {
+            for (int j = i; j < number_of_vertices; ++j) {
+                all_edges.push_back({i, j});
+            }
         }
-    }
 
-    // Generate all combinations of order edges
-    auto all_dashed_combinations = combinations(all_edges, order);
+        // Generate all combinations of order edges
+        auto all_dashed_combinations = combinations(all_edges, order);
 
-    for (int num_solid_edges = 0; num_solid_edges < max_solid_edges + 1; ++num_solid_edges) {
-        std::cout << std::to_string(num_solid_edges) << std::endl;
-        auto all_solid_combinations = combinations(all_edges, num_solid_edges);
         for (const auto& dashed_edges : all_dashed_combinations) {
+            auto all_solid_combinations = combinations(all_edges, number_of_vertices - 1);
+
             for (const auto& solid_edges : all_solid_combinations) {
                 // Initialize graph
-                std::tuple<SimpleGraph, std::vector<SimpleGraph::vertex_descriptor>> result = get_initial_graph_and_vertices(order);
+                std::tuple<SimpleGraph, std::vector<SimpleGraph::vertex_descriptor>> result = get_initial_graph_and_vertices(number_of_vertices);
 
                 SimpleGraph G;
                 std::vector<SimpleGraph::vertex_descriptor> vertices;
@@ -300,6 +386,7 @@ int main(int argc, char* argv[]) {
                     G[e].style = "dashed";
                 }
 
+
                 // Add solid edges
                 for (const auto& solid_edge : solid_edges) {
                     auto e = add_edge(vertices[solid_edge.first], vertices[solid_edge.second], G).first;
@@ -312,73 +399,69 @@ int main(int argc, char* argv[]) {
                     G[e].style = "solid";
                 }
 
+                // Perform DFS to find all vertices connected to vertices[0]
+                auto reachable_from_initial = dfs_reachable_vertices(G, vertices[0]);
+
+                for (const auto& v : vertices) {
+                    if (reachable_from_initial.find(v) == reachable_from_initial.end()) {
+                        graph_is_correct = false;
+                        break;
+                    }
+                }
+
+                if (!graph_is_correct) {
+                    continue;
+                }
+
                 // Labeling
                 for (const auto& v : vertices) {
                     int d = G[v].dashed_degree;
                     G[v].label = std::to_string(d);
                 }
 
+/*
                 if (G[vertices[0]].dashed_degree < 1 || G[vertices[1]].dashed_degree < 1) {
                     graph_is_correct = false;
                     continue;
                 }
-
-                // Perform BFS to find all vertices connected to vertices[0]
-                auto reachable_from_initial = bfs_reachable_vertices(G, vertices[0]);
+*/
 
                 for (const auto& v : vertices) {
-                    if (G[v].solid_degree != 0 || G[v].dashed_degree != 0) {
-                        if (reachable_from_initial.find(v) == reachable_from_initial.end()) {
-                            graph_is_correct = false;
-                            break;
-                        }
-                    }
-                }
-
-                if (!graph_is_correct) {
-                    continue;
-                }
-
-                std::vector<SimpleGraph::vertex_descriptor> connected_vertices;
-                for (const auto& v : vertices) {
-                    if (reachable_from_initial.find(v) != reachable_from_initial.end()) {
-                        connected_vertices.push_back(v);
-                    } else {
-                        remove_vertex(v, G);
-                    }
-                }
-
-                for (const auto& v : connected_vertices) {
                     if (G[v].solid_degree > 2) {
                         graph_is_correct = false;
+                        break;
                     }
                     if (G[v].dashed_degree == 0 && G[v].solid_degree != 0) {
                         graph_is_correct = false;
+                        break;
                     }
                     if (G[v].dashed_degree != 0 && G[v].solid_degree == 0) {
                         graph_is_correct = false;
+                        break;
                     }
 
                     if (G[v].solid_degree != G[v].required_solid_degree) {
                         graph_is_correct = false;
+                        break;
                     }
 
                     if (G[v].dashed_loop || G[v].solid_loop) {
                         graph_is_correct = false;
+                        break;
                     }
                 }
+
+                std::cout << "Hello, World" << std::endl;
 
                 if (!graph_is_correct) {
                     continue;
                 }
-
 
                 if (graph_is_correct) {
                     // graph_is_correct
                     std::ofstream file("dot/graph_" + std::to_string(file_counter++) + ".dot");
                     boost::write_graphviz(file, G, vertex_writer(G), edge_writer(G), graph_writer());
                 }
-
             }
         }
     }
