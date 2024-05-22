0021b20916522f9b036cdf46bc2fe5789ec940e5
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-22 09:57:28 +0900

    :zap: main.cpp | add def_reachable_vertices

--- src/main.cpp
+++ src/main.cpp
@@ -207,6 +207,31 @@ std::unordered_set<SimpleGraph::vertex_descriptor> bfs_reachable_vertices(const 
     return visited;
 }
 
+
+// DFS to find all vertices reachable from the start vertex
+std::unordered_set<SimpleGraph::vertex_descriptor> dfs_reachable_vertices(const SimpleGraph &G, SimpleGraph::vertex_descriptor start) {
+    std::unordered_set<SimpleGraph::vertex_descriptor> visited;
+    std::stack<SimpleGraph::vertex_descriptor> s;
+
+    visited.insert(start);
+    s.push(start);
+
+    while (!s.empty()) {
+        auto u = s.top();
+        s.pop();
+
+        for (auto edge_it = out_edges(u, G); edge_it.first != edge_it.second; ++edge_it.first) {
+            auto v = target(*edge_it.first, G);
+            if (visited.find(v) == visited.end()) {
+                visited.insert(v);
+                s.push(v);
+            }
+        }
+    }
+
+    return visited;
+}
+
 int main(int argc, char* argv[]) {
     // argc: Number of command-line args
     // argv: Array of command-line args
